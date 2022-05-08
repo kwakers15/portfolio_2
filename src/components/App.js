d17d38e869aebb0898c099ecce8473c8b1503dbe
Minjun Kwak <mk496@duke.edu>
2022-05-08 00:39:35 -0600

    Finished basic header, move on to nav

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './Header/Header';
+import Nav from './Nav/Nav';
+import About from './About/About';
+import Experience from './Experience/Experience';
+import Services from './Services/Services';
+import Portfolio from './Portfolio/Portfolio';
+import Testimonials from './Testimonials/Testimonials';
+import Contact from './Contact/Contact';
+import Footer from './Footer/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
